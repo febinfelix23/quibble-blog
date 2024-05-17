70154c94bfd5a2f16091dc7e91b2d5c47b48d4d5
febinfelix23 <febinfelix23@gmail.com>
2024-05-17 11:13:49 +0530

    make dashboard private

--- quibble/src/App.jsx
+++ quibble/src/App.jsx
@@ -7,20 +7,23 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Header from "./components/Header"
 import FooterComp from "./components/FooterComp"
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/projects" element={<Projects/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/login" element={<Login/>}/>
-    </Routes>
-    <FooterComp/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <FooterComp />
     </>
   )
 }
