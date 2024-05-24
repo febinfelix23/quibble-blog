2850346c5f542bab6a30af89a636298be3d6229d
febinfelix23 <febinfelix23@gmail.com>
2024-05-24 18:09:41 +0530

    add recent article section

--- quibble/src/pages/PostPage.jsx
+++ quibble/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CommentSection from '../components/CommentSection'
+import PostCard from '../components/PostCard'
 
 function PostPage() {
 
@@ -9,31 +10,47 @@ function PostPage() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [post, setPost] = useState(null)
+    const [recentPosts, setRecentPosts] = useState(null)
 
-    const fetchPost = async () => {
-        try {
-            setLoading(true);
-            const result = await fetch(`/qserver/post/getposts?slug=${postSlug}`);
-            const resultData = await result.json();
-            if (result.ok) {
-                setPost(resultData.posts[0])
-                setLoading(false)
-                setError(false)
-            } else {
-                setError(true)
-                setLoading(false)
-                console.log(error);
-                return
-            }
-        } catch (error) {
-            setError(true)
-            setLoading(false)
-        }
-    }
 
     useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                setLoading(true);
+                const result = await fetch(`/qserver/post/getposts?slug=${postSlug}`);
+                const resultData = await result.json();
+                if (result.ok) {
+                    setPost(resultData.posts[0])
+                    setLoading(false)
+                    setError(false)
+                } else {
+                    setError(true)
+                    setLoading(false)
+                    console.log(error);
+                    return
+                }
+            } catch (error) {
+                setError(true)
+                setLoading(false)
+            }
+        }
         fetchPost()
     }, [postSlug])
+
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async ()=>{
+                const result = await fetch('/qserver/post/getposts?limit=3')
+                const resultData = await result.json()
+                if(result.ok){
+                    setRecentPosts(resultData.posts)
+                }
+            }
+            fetchRecentPosts()
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [])
 
     if (loading) return (
         <div className="flex justify-center items-center min-h-screen">
@@ -45,9 +62,9 @@ function PostPage() {
             <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>{post && post.title}</h1>
 
             <Link to={`/search?category=${post && post.category}`} className='self-center mt-5'>
-            <Button color={'gray'} pill size={'xs'}>
-                {post && post.category}
-            </Button>
+                <Button color={'gray'} pill size={'xs'}>
+                    {post && post.category}
+                </Button>
             </Link>
 
             <img src={post && post.image} alt={post && post.title} className='mt-10 p-3 max-h-[600] w-full object-cover' />
@@ -57,9 +74,21 @@ function PostPage() {
                 <span>{post && (post.content.length / 1000).toFixed(0)} mins read</span>
             </div>
 
-            <div className='p-2 max-w-5xl mx-auto w-full mt-5 post-content' dangerouslySetInnerHTML={{__html: post && post.content}}></div>
-            
-            <CommentSection postId={post._id}/>
+            <div className='p-2 max-w-5xl mx-auto w-full mt-5 post-content' dangerouslySetInnerHTML={{ __html: post && post.content }}></div>
+
+            <CommentSection postId={post._id} />
+
+            <div className="flex flex-col justify-center items-center mb-5">
+                <h1 className='text-xl mt-5'>Recent Articles</h1>
+                <div className="flex flex-wrap gap-5 mt-5 justify-center">
+                    {
+                        recentPosts && 
+                        recentPosts.map((post)=>(
+                            <PostCard key={post._id} post={post} />
+                        ))
+                    }
+                </div>
+            </div>
         </main>
     )
 }
