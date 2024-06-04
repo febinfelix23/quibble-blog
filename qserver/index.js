14774b8dd151e3c393131f134a41fcf294326236
febinfelix23 <febinfelix23@gmail.com>
2024-06-04 16:12:51 +0530

    add pre installation

--- qserver/index.js
+++ qserver/index.js
@@ -3,9 +3,13 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import router from './routes/Routes.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 // dotenv config
 dotenv.config()
+
+// Directory
+const __dirname = path.resolve()
 
 // Initialize express server
 const qserver = express()
@@ -17,23 +21,30 @@ qserver.use(express.json())
 qserver.use(cookieParser())
 
 // Connect to Mongoose
-mongoose.connect(process.env.DATABASE).then(()=>{
+mongoose.connect(process.env.DATABASE).then(() => {
     console.log('MongoDB Atlas connected to Quibble server');
-}).catch((err)=>{
+}).catch((err) => {
     console.log(`MongoDB connection failed!! error:${err}`);
 })
 
 // Use routes
-qserver.use('/qserver',router)
+qserver.use('/qserver', router)
+
+// Use directory
+qserver.use(express.static(path.join(__dirname, '/quibble/dist')))
+
+qserver.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'quibble', 'dist', 'index.html'))
+})
 
 // Select Port
 const PORT = 3000 || process.env.PORT
 
-qserver.listen(PORT, ()=>{
+qserver.listen(PORT, () => {
     console.log(`Quibble Server running at Port ${PORT} and waiting for client request!`);
 })
 
-qserver.use((err, req, res, next)=>{
+qserver.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal server error"
     res.status(statusCode).json({
